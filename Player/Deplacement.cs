ff91b11594d2e872b26d8eb5a0c8a1f49f852759
Stephhawk <Stephen-and-co@hotmail.fr>
2019-01-26 00:26:42 +0100

    Deplacement <- ->
    Signed-off-by: Stephhawk <Stephen-and-co@hotmail.fr>

---
+++ Player/Deplacement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    public float vitesse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vitesse = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*vitesse , 0, 0);
+    }
+}
