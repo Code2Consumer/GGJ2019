57020a5b6c713f5db302d2d952f75268b13226a1
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2019-01-25 22:50:07 +0100

    Ajout des enemies et du floor
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

---
+++ Enemy/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+	public float walkSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walkLeft();
+    }
+
+    void walkLeft(){
+    	// transform.position.x = transform.position.x * 
+    	// ( walkSpeed * -1 );
+        transform.Translate(Vector3.left * Time.deltaTime * walkSpeed);
+    } 
+}
