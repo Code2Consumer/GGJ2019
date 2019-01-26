25e807fe1402b456d3691749cca65f558bd48cba
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2019-01-26 18:23:13 +0100

    Ajout des songs
    Signed-off-by: Zakaria Aboukhriss <zak@weddingplanner.co.uk>

--- HUD/TitleScreen/TitleScreenScript.cs
+++ HUD/TitleScreen/TitleScreenScript.cs
@@ -7,7 +7,7 @@ public class TitleScreenScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AkSoundEngine.PostEvent("Game_Start", gameObject);
     }
 
     // Update is called once per frame
---
+++ StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ATTACK_MISS = 1641587168U;
+        static const AkUniqueID GAME_START = 733168346U;
+        static const AkUniqueID KILL = 2030840469U;
+        static const AkUniqueID PLAYER_FOOTSTEP = 2453392179U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace AMBIANCE
+        {
+            static const AkUniqueID GROUP = 2981377429U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LANE1 = 3514553268U;
+                static const AkUniqueID LANE2 = 3514553271U;
+                static const AkUniqueID LANE3 = 3514553270U;
+            } // namespace STATE
+        } // namespace AMBIANCE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ECHELLE = 4003899189U;
+                static const AkUniqueID LANE1 = 3514553268U;
+                static const AkUniqueID LANE2 = 3514553271U;
+                static const AkUniqueID LANE3 = 3514553270U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SOUND_EFFECTS = 978636652U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
